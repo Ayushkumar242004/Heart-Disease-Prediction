7ff9856cbebdd09a0c6331920575970864b12086
Ayushkumar242004 <143803366+Ayushkumar242004@users.noreply.github.com>
2025-04-11 12:05:27 +0530

    added public backend url

--- components/prediction-form.tsx
+++ components/prediction-form.tsx
@@ -177,7 +177,7 @@ export default function PredictionForm({ onSubmit }: PredictionFormProps) {
         const formData = JSON.parse(storedData);
   
         // Send POST request to the API
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://heart-disease-prediction-backend-ji6p.onrender.com/predict", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
